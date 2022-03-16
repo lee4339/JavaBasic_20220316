a2dbe4d52c4e55dc9e4c971f122fdd36903f4aca
lee4339 <skyho4339@naver.com>
2022-03-16 21:58:21 +0900

    22.03.16 수업내용

---
+++ JavaStudy_4084_Basic/src/j01_hello/HelloJava.java
@@ -0,0 +1,23 @@
+package j01_hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.print("hello, JAVA\n");
+		System.out.print("이건호");
+		
+		/*
+		 * 과제1
+		 * 이름: 이건호
+		 * 연락처: 010-9307-4084
+		 * 이메일: aaaa@naver.com
+		 * 주소: 부산광역시 사하구
+		 * 
+		 */
+		/*
+		 * 과제2
+		 * jdk 글 출력
+		 */
+	}
+
+}
