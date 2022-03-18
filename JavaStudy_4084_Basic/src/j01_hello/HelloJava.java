03c5c8ef2e532c7012809adb9e8053d16fdf8525
lee4339 <skyho4339@naver.com>
2022-03-18 18:50:29 +0900

    첫날 수업 과제완료

--- JavaStudy_4084_Basic/src/j01_hello/HelloJava.java
+++ JavaStudy_4084_Basic/src/j01_hello/HelloJava.java
@@ -6,11 +6,13 @@ public class HelloJava {
 		System.out.print("hello, JAVA\n");
 		System.out.print("이건호");
 		
+		System.out.println();
+		System.out.println();
 		/*
 		 * 과제1
 		 * 이름: 이건호
 		 * 연락처: 010-9307-4084
-		 * 이메일: aaaa@naver.com
+		 * 이메일: skyho4339@naver.com
 		 * 주소: 부산광역시 사하구
 		 * 
 		 */
@@ -18,6 +20,32 @@ public class HelloJava {
 		 * 과제2
 		 * jdk 글 출력
 		 */
+		
+		System.out.print("[과제1]\n");
+		System.out.print("이름: ");
+		System.out.print("이건호\n");
+		System.out.print("연락처: ");
+		System.out.print("010-9307-4084\n");
+		System.out.print("이메일: ");
+		System.out.print("skyho4339@naver.com\n");
+		System.out.print("주소: ");
+		System.out.print("부산광역시 사하구\n");
+		System.out.println();
+		
+		System.out.println("[과제2]");
+		System.out.println("Important Oracle JDK License Update");
+		System.out.println();
+		System.out.println("The Oracle JDK License has changed for releases starting April 16, 2019.");
+		System.out.print("The new Oracle Technology Network License Agreement for Oracle Java SE is ");
+		System.out.println("substantially different from prior Oracle JDK licenses.");
+		System.out.print("The new license permits certain, such as personal use and development use, at no cost -- ");
+		System.out.println("but other uses authorized under prior Oracle JDK licenses may no longer be avaliable.");
+		System.out.println("Please review the terms carefully before downloading and using this product.");
+		System.out.println("An FAQ is avaliable here.");
+		System.out.println();
+		System.out.println("Commercial license and support is available with a low cost Java SE Subscription.");
+		System.out.println();
+		System.out.println("Oracle also provides the latest OpenJDK release under the open source GPL License at jdk.java.net.");
 	}
 
 }
