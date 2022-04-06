5d210b8531113540d474f777c57cd4e304f55d61
lee4339 <skyho4339@naver.com>
2022-04-06 21:48:50 +0900

    22.04.06 수업내용

---
+++ JavaStudy_4084_Basic/src/J11_클래스_접근지정/UserTest.java
@@ -0,0 +1,14 @@
+package J11_클래스_접근지정;
+
+public class UserTest {
+
+	public static void main(String[] args) {
+		User_J11 u1 = new User_J11();
+		u1.setUsername("geonho");
+		//String username = u1.getUsername();
+		System.out.println(u1.getUsername());
+		
+
+	}
+
+}
